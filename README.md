f8269e3ba2f3f52580abc8c526f9b6d06d8082ec
starSmallDream <xhc57718>
2021-03-02 20:12:40 +0800

    SpringCloud-ConfigServer端

---
+++ README.md
@@ -0,0 +1,7 @@
+## 介绍
+此为SpringCloud例子中的配置服务端，后续会陆续补充其他SpringCloud其余项目，进行整合
+
+
+
+## 引用
+SpringCloudConfig官网：https://docs.spring.io/spring-cloud-config/docs/current/reference/html
---
+++ src/main/java/com/example/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${info.foo}")
+    private String bar;
+
+    @GetMapping
+    public String home(){
+        return bar;
+    }
+
+}
